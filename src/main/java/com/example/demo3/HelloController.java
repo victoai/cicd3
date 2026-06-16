940993fff3f340d1816c98e457a49a80bb512512
victoai <victoai@naver.com>
2026-06-16 14:57:50 +0900

    cicid3

---
+++ src/main/java/com/example/demo3/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo3;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hi-cicd")
+
+    public String  hi(){
+
+        return  "hi  cicd docker   ECR !!!  ^^^ !!! 0616 ";
+    }
+}
